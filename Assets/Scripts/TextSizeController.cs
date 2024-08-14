774dfc4bb334469e5df39946343881e2da6a0df3
Sergey Chaliy <70662544+sergeychaliy2@users.noreply.github.com>
2024-08-14 12:50:25 +0300

    upd panel settings

--- Assets/Scripts/TextSizeController.cs
+++ Assets/Scripts/TextSizeController.cs
@@ -3,14 +3,12 @@ using TMPro;
 
 public class TextSizeController : MonoBehaviour
 {
-    public TMP_Text textMeshPro;
-
-    public float fontSizeStep = 2f;
-    public float minFontSize = 10f;
-    public float maxFontSize = 100f;
-
-    // Минимальная допустимая разница между текущим размером шрифта и целевым (для борьбы с неточностями float).
-    private float epsilon = 0.01f;
+    [SerializeField] private TMP_Text textMeshPro;
+    [SerializeField] private float fontSizeStep = 2f;
+    [SerializeField] private float minFontSize = 10f;
+    [SerializeField] private float maxFontSize = 100f;
+    [SerializeField] private GameObject panel;
+    [SerializeField] private float epsilon = 0.01f;
 
     public void IncreaseFontSize()
     {
@@ -18,7 +16,6 @@ public class TextSizeController : MonoBehaviour
         {
             float newSize = textMeshPro.fontSize + fontSizeStep;
 
-            // Проверка на возможные неточности и лимиты.
             if (newSize > textMeshPro.fontSize && newSize <= maxFontSize)
             {
                 textMeshPro.fontSize = Mathf.Min(newSize, maxFontSize);
@@ -32,11 +29,18 @@ public class TextSizeController : MonoBehaviour
         {
             float newSize = textMeshPro.fontSize - fontSizeStep;
 
-            // Проверка на возможные неточности и лимиты.
             if (newSize < textMeshPro.fontSize && newSize >= minFontSize)
             {
                 textMeshPro.fontSize = Mathf.Max(newSize, minFontSize);
             }
+        }
+    }
+
+    public void TogglePanel()
+    {
+        if (panel != null)
+        {
+            panel.SetActive(!panel.activeSelf);
         }
     }
 }
